60a271753d3b47334d0a9f517e978635f7521cad
soumili202 <soumilirupsa2020@gmail.com>
2024-03-02 12:34:19 +0530

    Initialized the frontend

---
+++ frontend/front-end/src/App.js
@@ -0,0 +1,12 @@
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h className="text-3xl font-serif font-bold">Hello World</h>
+    </div>
+  );
+}
+
+export default App;
